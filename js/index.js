46d42a7e4d371325fd40eb097eb6c3f2483ab9fe
Fernando Montes <Fernando.m.l426@gmail.com>
2023-08-09 22:37:55 -0400

    Fixed linter errors
    Fixed linters errors in js file

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 const opneNave = () => {
   document.getElementById('mobileNave').style.width = '100%';
 };
@@ -9,46 +8,45 @@ const closeNave = () => {
 opneNave();
 closeNave();
 
-
 // _______________Dinamic Content___________________
 
 const artistsData = [
   {
-    name: "Shepard Fairey",
-    description: "Street artist, graphic designer, and activist.",
-    imageSrc: "/images/artis01 shepard farei.jpg",
+    name: 'Shepard Fairey',
+    description: 'Street artist, graphic designer, and activist.',
+    imageSrc: '/images/artis01 shepard farei.jpg',
     bio: "Fairey gained widespread recognition for his 'Hope' poster featuring Barack Obama during the 2008.",
   },
   {
-    name: "JR",
-    description: "Photographer known for large-scale public art projects.",
-    imageSrc: "/images/JR artist 02JPG.JPG",
-    bio: "His works often involve portraits of ordinary people in marginalized communities.",
+    name: 'JR',
+    description: 'Photographer known for large-scale public art projects.',
+    imageSrc: '/images/JR artist 02JPG.JPG',
+    bio: 'His works often involve portraits of ordinary people in marginalized communities.',
   },
   {
-    name: "Ai Weiwei",
-    description: "Chinese artist, and dissident whose work tackles issues of human rights.",
-    imageSrc: "/images/Ai Weiwei Artis 03.JPG",
-    bio: "He works across various media, including installations, sculptures, and photography.",
+    name: 'Ai Weiwei',
+    description: 'Chinese artist, and dissident whose work tackles issues of human rights.',
+    imageSrc: '/images/Ai Weiwei Artis 03.JPG',
+    bio: 'He works across various media, including installations, sculptures, and photography.',
   },
   {
-    name: "Yayoi Kusama",
-    description: "Japanese contemporary artist known for her immersive art installations.",
-    imageSrc: "/images/yayoi kusama artis 04.JPG",
-    bio: "Her works often feature repetitive patterns and explore themes of infinity and self-obliteration.",
+    name: 'Yayoi Kusama',
+    description: 'Japanese contemporary artist known for her immersive art installations.',
+    imageSrc: '/images/yayoi kusama artis 04.JPG',
+    bio: 'Her works often feature repetitive patterns and explore themes of infinity and self-obliteration.',
   },
   {
-    name: "Barbara Kruger",
-    description: "Artist and designer famous for her use of text in her artworks.",
-    imageSrc: "/images/Barbara Kruger artis 05.JPG",
-    bio: "Her pieces often address issues of gender, consumerism, and power, frequently featuring bold, large-lettered phrases.",
+    name: 'Barbara Kruger',
+    description: 'Artist and designer famous for her use of text in her artworks.',
+    imageSrc: '/images/Barbara Kruger artis 05.JPG',
+    bio: 'Her pieces often address issues of gender, consumerism, and power, frequently featuring bold, large-lettered phrases.',
   },
   {
-    name: "Kehinde Wiley",
-    description: "American painter known for his contemporary portraits.",
-    imageSrc: "/images/kehinde Wiley.JPG",
-    bio: "Incorporating Black subjects in heroic and ornate poses, exploring representation and identity.",
-  }
+    name: 'Kehinde Wiley',
+    description: 'American painter known for his contemporary portraits.',
+    imageSrc: '/images/kehinde Wiley.JPG',
+    bio: 'Incorporating Black subjects in heroic and ornate poses, exploring representation and identity.',
+  },
 ];
 
 function generateArtistHTML(artist) {
@@ -68,10 +66,10 @@ function generateArtistHTML(artist) {
 }
 
 function populateArtistsContainer() {
-  const artistsContainer = document.getElementById("artistsContainer");
-  
-  let artistsHTML = "";
-  artistsData.forEach(artist => {
+  const artistsContainer = document.getElementById('artistsContainer');
+
+  let artistsHTML = '';
+  artistsData.forEach((artist) => {
     artistsHTML += generateArtistHTML(artist);
   });
 
